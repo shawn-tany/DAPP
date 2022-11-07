1916ed3efdd85609936981c8c224f458cbc49c56
txy <375317318@qq.com>
2022-11-07 02:17:05 -0800

    [add/modify] dpdk share library&port module

--- lib/lib.mk
+++ lib/lib.mk
@@ -4,11 +4,13 @@ LIB_DPDK_DIR = $(LIB_DIR)/dpdk
 LIB_JANSSON_DIR = $(LIB_DIR)/jansson
 LIB_HYPERSCAN_DIR = $(LIB_DIR)/hyperscan
 
-LIBS += $(LIB_DPDK_DIR)/lib/libdpdk.a
-LIBS += $(LIB_JANSSON_DIR)/lib/libjansson.a
-
 CFLAGS += -I $(LIB_DPDK_DIR)/include
 CFLAGS += -I $(LIB_JANSSON_DIR)/include
-CFLAGS += -lpthread -lnuma -ldl
+CFLAGS += -lpthread -lnuma -ldl -march=native
+
+CFLAGS += -L $(LIB_JANSSON_DIR)/lib -ljansson -Wl,-rpath=$(LIB_JANSSON_DIR)/lib
 CFLAGS += -L $(LIB_HYPERSCAN_DIR)/lib -lhs -Wl,-rpath=$(LIB_HYPERSCAN_DIR)/lib
-CFLAGS += -march=native
+CFLAGS += -L $(LIB_DPDK_DIR)/lib -ldpdk -Wl,-rpath=$(LIB_DPDK_DIR)/lib
+
+# Ethernet devices can also be found without using this compilation option
+#CFLAGS += -L $(LIB_DPDK_DIR)/lib -lrte_pmd_e1000 -Wl,-rpath=$(LIB_DPDK_DIR)/lib
--- source/modules/port/port.c
+++ source/modules/port/port.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include "modules.h"
+
 #include "rte_ethdev.h"
+#include "rte_mbuf.h"
+
+#define MAC_PRINT(mac) printf("%02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])
 
 typedef struct 
 {
-    
+    UINT16_T n_rx_ports;
+    UINT16_T n_tx_ports;
+
+    struct rte_mempool *rx_mempool;
 } dapp_port_ws_t;
+
+static struct rte_eth_conf dev_conf_default = {
+    .rxmode = {
+        .max_rx_pkt_len = RTE_ETHER_MAX_LEN,
+    }
+};
+
+static dapp_port_ws_t port_ws;
 
 static void dapp_port_loop(void)
 {
     dapp_module_t *port_module = NULL;
+    struct rte_mbuf *mbuff[8];
 
     port_module = dapp_module_get_by_type(DAPP_MODULE_PORT);
 
@@ -22,19 +38,103 @@ static void dapp_port_loop(void)
      * Port loop
      */
     while (port_module->lcore.running) {
-        DAPP_TRACE("dapp port loop\n");
-        sleep(3);
+
+        UINT16_T port_id;
+        UINT16_T nb_rx;
+
+        RTE_ETH_FOREACH_DEV(port_id) {
+            nb_rx = rte_eth_rx_burst(port_id, 0, mbuff, 8);
+
+            if (0 == nb_rx) {
+                continue;
+            }
+
+            printf("rx pkts %d\n", nb_rx);
+        }
     }
 }
 
-int dapp_port_init(void *arg)
+static int dapp_port_init(void *arg)
 {
     DAPP_TRACE("dapp port init\n");
+
+    int ret;
+
+    UINT16_T n_ports = rte_eth_dev_count_avail();
+
+    if (0 == n_ports) {
+        printf("No ethernet device avalible!\n");
+        return -1;
+    }
+    
+    port_ws.n_rx_ports = 1;
+    port_ws.n_tx_ports = 1;
+
+    /*
+     * Create rx mbuf pool
+     */
+    port_ws.rx_mempool = rte_pktmbuf_pool_create("DAPP_RX_MPOOL", 10240, 0, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+    if (!port_ws.rx_mempool) {
+        printf("pktmbuf pool %s create fail!\n", "DAPP_RX_MPOOL");
+        return -1;
+    }
+
+    /*
+     * Initialize all port
+     */
+    UINT16_T port_id;
+    
+    RTE_ETH_FOREACH_DEV(port_id) {
+        /*
+         * Configure port
+         */
+        ret = rte_eth_dev_configure(port_id, port_ws.n_rx_ports, port_ws.n_tx_ports, &dev_conf_default);
+        if (0 > ret) {
+            printf("Can not configure device! ERR : %d\n", ret);
+            return -1;
+        }
+
+        /*
+         * Setup rx queue
+         */
+        ret = rte_eth_rx_queue_setup(port_id, 0, 128, rte_eth_dev_socket_id(port_id), NULL, port_ws.rx_mempool);
+        if (0 > ret) {
+            printf("Can not setup rx queue! ERR : %d\n", ret);
+            return -1;
+        }
+
+        /*
+         * Setup tx queue
+         */
+        ret = rte_eth_tx_queue_setup(port_id, 0, 128, rte_eth_dev_socket_id(port_id), NULL);
+        if (0 > ret) {
+            printf("Can not setup tx queue! ERR : %d\n", ret);
+            return -1;
+        }
+
+        rte_eth_dev_start(port_id);
+
+        /*
+         * enable port promiscuous mode
+         */
+        ret = rte_eth_promiscuous_enable(port_id);
+        if (0 > ret) {
+            printf("Can not enable promiscuous mode\n");
+            return -1;
+        }
+
+        /*
+         * Show mac address of port
+         */
+        struct rte_ether_addr mac_addr;
+        rte_eth_macaddr_get(port_id, &mac_addr);
+        MAC_PRINT(mac_addr.addr_bytes);
+    }
 
     return DAPP_OK;
 }
 
-int dapp_port_exec(void *arg)
+static int dapp_port_exec(void *arg)
 {
     DAPP_TRACE("dapp port exec\n");
 
@@ -43,7 +143,7 @@ int dapp_port_exec(void *arg)
     return DAPP_OK;
 }
 
-int dapp_port_exit(void *arg)
+static int dapp_port_exit(void *arg)
 {
     DAPP_TRACE("dapp port exit\n");
 
